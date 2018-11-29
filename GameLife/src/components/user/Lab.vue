5206da34ca3f92dc289c0d53d250c4d19782aa9d
戴瑜灿 <763502583@qq.com>
2018-11-29 23:05:09 +0800

    修改研究模块

--- GameLife/src/components/user/Lab.vue
+++ GameLife/src/components/user/Lab.vue
@@ -1,15 +1,31 @@
 <template>
   <Card>
     <h2>仿生人研究</h2>
-    <Button>外貌研究</Button>
-    <Button>性格研究</Button>
-    <Button>能力研究</Button>
+    <Tabs value="name1">
+      <TabPane :label="label" name="name1">外貌研究</TabPane>
+      <TabPane label="性格研究" name="name2">性格研究</TabPane>
+      <TabPane label="能力研究" name="name3">能力研究</TabPane>
+    </Tabs>
   </Card>
 </template>
 
 <script>
 export default {
-  name: 'Lab'
+  name: 'Lab',
+  data () {
+    return {
+      label: (h) => {
+        return h('div', [
+          h('span', '外貌研究'),
+          h('Badge', {
+            props: {
+              count: 3
+            }
+          })
+        ])
+      }
+    }
+  }
 }
 </script>
 
